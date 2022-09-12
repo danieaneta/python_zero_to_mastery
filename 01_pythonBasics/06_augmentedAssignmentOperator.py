82b120a9f8832f2f04a27b9fa5bc82a5bf2b9d79
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 14:32:53 -0500

    Update 06_augmentedAssignmentOperator.py

--- 01_pythonBasics/06_augmentedAssignmentOperator.py
+++ 01_pythonBasics/06_augmentedAssignmentOperator.py
@@ -24,3 +24,18 @@ print(some_value)
 # +=
 # -=
 # *=
+print('_______________________________________________')
+print('AUGMENTED ASSIGNMENT OPERATOR ASSIGNMENT: ')
+
+
+counter = 0
+
+counter += 1
+counter += 1
+counter += 1
+counter += 1
+counter -= 1
+counter *=2
+
+#Before you click RUN, guess what the counter variable holds in memory!
+print(counter)
