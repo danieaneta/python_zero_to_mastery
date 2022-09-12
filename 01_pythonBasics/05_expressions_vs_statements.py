a6fddbfd06f019f36f4534184f0bf61076ed61f7
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 14:21:38 -0500

    Create 05_expressions_vs_statements.py

---
+++ 01_pythonBasics/05_expressions_vs_statements.py
@@ -0,0 +1,7 @@
+#EXPRESSIONS VS STATEMENTS
+
+iq = 100
+user_age = iq / 5
+
+#iq / 5 is an expression - a piece of code that produces value
+# user_age = iq / 5 - an entire line of code that performs some sort of action. ie. calculating "iq / 5" then assigning that value to user_age
