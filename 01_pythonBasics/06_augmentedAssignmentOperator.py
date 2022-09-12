1e8774e20f4b5524119b3fca2e3230028e369224
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 14:29:36 -0500

    Create 06_augmentedAssignmentOperator.py

---
+++ 01_pythonBasics/06_augmentedAssignmentOperator.py
@@ -0,0 +1,26 @@
+#AUGMENTED ASSIGNMENT OPERATOR
+
+#Variable MUST be defined before using augmented assignment operator
+
+some_value = 5
+some_value = some_value + 2
+print('some_value = 5')
+print('some_value = some_value + 2')
+print('This is NOT using Augmented Assignment Operator: ')
+print(some_value)
+
+print('_______________________________________________')
+#instead we can use the augmented assignment operator
+
+some_value = 5
+some_value += 2
+print('some_value = 5')
+print('some_value += 2')
+print('This is using Augmented Assignment Operator: ')
+print(some_value)
+
+#can also change it to '-=' to subtract
+
+# +=
+# -=
+# *=
