f4899d2aa69c26f82243c04e22183a7b3f278454
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 13:02:47 -0500

    Create 03_complex_binI()_optional.py

---
+++ 01_pythonBasics/03_complex_binI()_optional.py
@@ -0,0 +1,16 @@
+#Extra data type
+
+print('complex')
+complex
+#Used only for complex math
+
+# bin () - return binary representation of an integer
+
+print(bin(5))
+print('Answer: 0b101')
+print('INT 5 = Binary Number 101')
+print('This is the binary representation of the INT 5.')
+
+print('Converting Binary Number to INT')
+print('print(int(0b101))')
+print(int(0b101))
