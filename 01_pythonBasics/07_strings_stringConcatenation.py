fb12b2a10dea0507de208acffd4e21a601cec613
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 23:00:18 -0500

    Create 07_strings_stringConcatenation.py

---
+++ 01_pythonBasics/07_strings_stringConcatenation.py
@@ -0,0 +1,26 @@
+# STRINGS
+
+# SINGLE AND DOUBLE QUOTES
+
+print(type('Hello'))
+username = 'danie'
+password = 'daniepassword'
+
+
+#LONG-STRING
+print('LONG STRING')
+long_string = '''
+this 
+is a 
+long string.
+'''
+
+print(long_string)
+
+#STRING CONCATENATION - Adding two strings together
+
+first_name = 'danielle'
+last_name = 'araneta'
+full_name = first_name + ' ' + last_name
+
+print(full_name)
