9085b4bb5d16eb376be8381c57b2e8f31ffe926f
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-12 23:21:49 -0500

    Create 09_escapeSequence.py

---
+++ 01_pythonBasics/09_escapeSequence.py
@@ -0,0 +1,19 @@
+#ESCAPE SEQUENCE
+
+#WILL NOT WORK
+weather = 'It's sunny'
+
+#WILL WORK
+weather = "It's sunny"
+
+#WILL NOT WORK
+weather = "It's "kind of" sunny"
+
+#USE ESCAPE SEQUENCE - WILL WORK
+weather = "It\'s \"kind of\" sunny"
+
+#ADDING A TAB '\t'
+weather = "\t It\'s \"kind of\" sunny"
+
+#ADDING A NEW LINE '\n'
+weather = "\t It\'s \"kind of\" sunny \n hope you have a good day."
