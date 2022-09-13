321b363391c01cfb7872b48702fcc9b54c81e593
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-09-13 00:24:53 -0500

    Update and rename 11_stringIndexes.py to 11_stringIndexes_immutability.py

--- 01_pythonBasics/11_stringIndexes_immutability.py
+++ 01_pythonBasics/11_stringIndexes_immutability.py
@@ -3,6 +3,9 @@
 string_index = '01234567'
             #   01234567
 
+print('STRING SLICING')
+#STRING SLICING
+
 # [START:STOP]
 print('print(string_index[0:3])')
 print(string_index[0:3])
@@ -34,3 +37,18 @@ print(string_index[-1])
 print(string_index[-2])
 print(string_index[-3])
 print(string_index[::-1])
+
+#IMMUTABILITY
+
+#STRINGS IN PYTHON ARE IMMUTABLE - THIS MEANS THEY CANNOT BE CHANGED
+
+string_index = '01234567'
+#THIS WILL NOT WORK - STRINGS ARE IMMUTABLE
+# string_index[0] = '8'
+print("string_index[0] = '8'")
+#THE ONLY WAY TO CHANGE STRING_INDEX IS TO REASSIGN THE VALUE
+
+#CAN DO
+string_index = string_index + '8'
+
+print(string_index)
